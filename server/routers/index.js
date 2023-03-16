dbff5af4b54e6af5ad4dbbf2c60edee7b24baa75
Agiff <ghifarimohammadrahadian@gmail.com>
2023-03-16 21:02:17 +0700

    feat: add CRUD for category

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const errorHandler = require('../middlewares/errorHandler');
 const userRouter = require('./users');
 const itemRouter = require('./items');
+const categoryRouter = require('./categories');
 const authentication = require('../middlewares/authentication');
 
 router.use('/users', userRouter);
 router.use('/items', authentication, itemRouter);
+router.use('/categories', categoryRouter);
 
 router.use(errorHandler);
 
